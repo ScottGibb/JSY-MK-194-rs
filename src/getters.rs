a03f130fe0f6b682b273297f8ea1d871a686e041
Scott Gibb <smgibb@yahoo.com>
2026-04-07 22:42:53 +0100

    feat: add getter templates
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/getters.rs
+++ src/getters.rs
@@ -1,4 +1,6 @@
 use crate::jsy_mk_194g::JsyMk194g;
+use crate::registers::system_configuration_paramater::Baudrate;
+use crate::types::SystemParameters;
 use crate::{
     error::JSYMk194Error,
     types::{Channel, ChannelStatistics, Statistics},
@@ -11,7 +13,6 @@ impl<Serial: Read + Write> JsyMk194g<Serial> {
         Ok(Statistics {
             channel_one: todo!(),
             channel_two: todo!(),
-            power_direction: todo!(),
             frequency: todo!(),
         })
     }
@@ -32,10 +33,30 @@ impl<Serial: Read + Write> JsyMk194g<Serial> {
             positive_active_energy: todo!(),
             negative_active_energy: todo!(),
             power_factor: todo!(),
+            power_direction: todo!(),
         })
     }
-
+    #[maybe_async::maybe_async]
     pub async fn get_frequency(&mut self) -> Result<Frequency, JSYMk194Error> {
+        todo!();
         Ok(Frequency::new::<hertz>(10.0))
+    }
+}
+
+impl<Serial: Read + Write> JsyMk194g<Serial> {
+    #[maybe_async::maybe_async]
+    pub async fn get_baudrate(&mut self) -> Result<Baudrate, JSYMk194Error> {
+        todo!();
+        Ok(Baudrate::_1200)
+    }
+
+    #[maybe_async::maybe_async]
+    pub async fn get_system_parameters(&mut self) -> Result<SystemParameters, JSYMk194Error> {
+        todo!();
+        Ok(SystemParameters {
+            ModelOne: 0,
+            voltage_range: ElectricPotential::new::<volt>(0.0),
+            current_range: ElectricCurrent::new::<ampere>(0.0),
+        })
     }
 }
