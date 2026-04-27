916d7d0d3ccb66a995bf32b55260b0ecdc8d7e80
Scott Gibb <smgibb@yahoo.com>
2026-04-27 22:06:09 +0100

    feat(tests): add remaining setter tests
    The active energy tests are now added
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- tests/setters_tests.rs
+++ tests/setters_tests.rs
@@ -1,6 +1,8 @@
 mod common;
 use common::setup_device;
 use jsy_mk_194_rs::types::{Baudrate, Id};
+use jsy_mk_194_rs::{types::Channel, units::Energy, units::kilowatt_hour};
+
 mod configuration_tests {
 
     use super::*;
@@ -73,4 +75,182 @@ mod configuration_tests {
 }
 mod energy_tests {
     use super::*;
+
+    #[test]
+    fn test_set_first_channel_positive_active_energy() {
+        let mut device = setup_device(Id::default(), Baudrate::default());
+        let new_active_energy = Energy::new::<kilowatt_hour>(123.45);
+
+        let original_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics")
+            .positive_active_energy;
+        println!(
+            "Original Active Energy: {:?} kWh",
+            original_active_energy.get::<kilowatt_hour>()
+        );
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the read request
+        device
+            .set_positive_active_energy(Channel::One, new_active_energy)
+            .expect("Failed to set new active energy");
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the write request
+
+        let updated_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics after update")
+            .positive_active_energy;
+        println!(
+            "Updated Active Energy: {:?} kWh",
+            updated_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(updated_active_energy, new_active_energy);
+
+        // Reset the active energy back to the default value so it doesn't affect other tests
+
+        device
+            .set_positive_active_energy(Channel::One, original_active_energy)
+            .expect("Failed to reset active energy");
+        let reset_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics after reset")
+            .positive_active_energy;
+        println!(
+            "Reset Active Energy: {:?} kWh",
+            reset_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(reset_active_energy, original_active_energy);
+    }
+
+    #[test]
+    fn test_set_first_channel_negative_active_energy() {
+        let mut device = setup_device(Id::default(), Baudrate::default());
+        let new_active_energy = Energy::new::<kilowatt_hour>(123.45);
+
+        let original_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics")
+            .negative_active_energy;
+        println!(
+            "Original Active Energy: {:?} kWh",
+            original_active_energy.get::<kilowatt_hour>()
+        );
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the read request
+        device
+            .set_negative_active_energy(Channel::One, new_active_energy)
+            .expect("Failed to set new active energy");
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the write request
+
+        let updated_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics after update")
+            .negative_active_energy;
+        println!(
+            "Updated Active Energy: {:?} kWh",
+            updated_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(updated_active_energy, new_active_energy);
+
+        // Reset the active energy back to the default value so it doesn't affect other tests
+
+        device
+            .set_negative_active_energy(Channel::One, original_active_energy)
+            .expect("Failed to reset active energy");
+        let reset_active_energy = device
+            .get_channel(Channel::One)
+            .expect("Failed to read channel one statistics after reset")
+            .negative_active_energy;
+        println!(
+            "Reset Active Energy: {:?} kWh",
+            reset_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(reset_active_energy, original_active_energy);
+    }
+
+    #[test]
+    fn test_set_second_channel_positive_active_energy() {
+        let mut device = setup_device(Id::default(), Baudrate::default());
+        let new_active_energy = Energy::new::<kilowatt_hour>(123.45);
+
+        let original_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics")
+            .positive_active_energy;
+        println!(
+            "Original Active Energy: {:?} kWh",
+            original_active_energy.get::<kilowatt_hour>()
+        );
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the read request
+        device
+            .set_positive_active_energy(Channel::Two, new_active_energy)
+            .expect("Failed to set new active energy");
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the write request
+
+        let updated_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics after update")
+            .positive_active_energy;
+        println!(
+            "Updated Active Energy: {:?} kWh",
+            updated_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(updated_active_energy, new_active_energy);
+
+        // Reset the active energy back to the default value so it doesn't affect other tests
+        device
+            .set_positive_active_energy(Channel::Two, original_active_energy)
+            .expect("Failed to reset active energy");
+        let reset_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics after reset")
+            .positive_active_energy;
+        println!(
+            "Reset Active Energy: {:?} kWh",
+            reset_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(reset_active_energy, original_active_energy);
+    }
+
+    #[test]
+    fn test_set_second_channel_negative_active_energy() {
+        let mut device = setup_device(Id::default(), Baudrate::default());
+        let new_active_energy = Energy::new::<kilowatt_hour>(123.45);
+
+        let original_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics")
+            .negative_active_energy;
+        println!(
+            "Original Active Energy: {:?} kWh",
+            original_active_energy.get::<kilowatt_hour>()
+        );
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the read request
+        device
+            .set_negative_active_energy(Channel::Two, new_active_energy)
+            .expect("Failed to set new active energy");
+        std::thread::sleep(jsy_mk_194_rs::REQUEST_RESPONSE_DELAY); // Give the device some time to process the write request
+
+        let updated_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics after update")
+            .negative_active_energy;
+        println!(
+            "Updated Active Energy: {:?} kWh",
+            updated_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(updated_active_energy, new_active_energy);
+
+        // Reset the active energy back to the default value so it doesn't affect other tests
+        device
+            .set_negative_active_energy(Channel::Two, original_active_energy)
+            .expect("Failed to reset active energy");
+        let reset_active_energy = device
+            .get_channel(Channel::Two)
+            .expect("Failed to read channel two statistics after reset")
+            .negative_active_energy;
+        println!(
+            "Reset Active Energy: {:?} kWh",
+            reset_active_energy.get::<kilowatt_hour>()
+        );
+        assert_eq!(reset_active_energy, original_active_energy);
+    }
 }
