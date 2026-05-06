00dbcafb6efa3f8759ca1f5981f215c4f843a3c9
Scott Gibb <smgibb@yahoo.com>
2026-05-06 22:27:53 +0100

    feat: add gettters example
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- examples/getters.rs
+++ examples/getters.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use jsy_mk_194_rs::jsy_mk_194g::JsyMk194g;
 use jsy_mk_194_rs::registers::system_configuration_paramater::Baudrate;
+use jsy_mk_194_rs::types::Channel;
 use jsy_mk_194_rs::units::*;
 fn main() {
     let ports = serialport::available_ports().expect("No ports found!");
@@ -9,7 +10,7 @@ fn main() {
     for p in ports {
         println!("  {}", p.port_name);
     }
-
+    println!("------------------------");
     // Open the first available port
     let port_name = "/dev/tty.usbserial-0001";
     let port = serialport::new(port_name, u32::from(Baudrate::default()))
@@ -20,7 +21,7 @@ fn main() {
     let delay = utils::StdDelay;
 
     let mut driver =
-        JsyMk194g::new_default(port, delay).expect(" The Device shoudl be on the port");
+        JsyMk194g::new_default(port, delay).expect(" The Device should be on this port");
 
     let system_parameters = driver
         .get_system_parameters()
@@ -32,4 +33,38 @@ fn main() {
 
     let frequency = driver.get_frequency().expect("Failed to read frequency");
     println!("Frequency: {} Hz", frequency.get::<hertz>());
+
+    println!("------------------------");
+    println!("Reading all channels...");
+    let stats = driver
+        .get_all_channels()
+        .expect("Failed to read all channels");
+    println!("Statistics: {stats}");
+
+    println!("------------------------");
+    println!("Reading channels individually...");
+    let channel_one = driver
+        .get_channel(Channel::One)
+        .expect("Failed to read channel one");
+    println!("Channel One: {channel_one}");
+
+    let channel_two = driver
+        .get_channel(Channel::Two)
+        .expect("Failed to read channel two");
+    println!("Channel Two: {channel_two}");
+
+    println!("------------------------");
+    let frequency = driver.get_frequency().expect("Failed to read frequency");
+    println!("Frequency: {} Hz", frequency.get::<hertz>());
+
+    println!("------------------------");
+    let power_direction = driver
+        .get_power_direction(Channel::One)
+        .expect("Failed to read power direction");
+    println!(" Channel One Power Direction: {power_direction:?}");
+
+    let power_direction = driver
+        .get_power_direction(Channel::Two)
+        .expect("Failed to read power direction");
+    println!(" Channel Two Power Direction: {power_direction:?}");
 }
