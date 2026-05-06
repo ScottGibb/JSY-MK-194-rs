1bb6d3a0c35617a8aafa8ee30a0838ccbac61554
Scott Gibb <smgibb@yahoo.com>
2026-05-06 22:28:05 +0100

    fix: remove print statements from codebase
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/modbus/buffer_communications.rs
+++ src/modbus/buffer_communications.rs
@@ -8,10 +8,10 @@ impl<Serial: Read + Write, D: DelayNs> JsyMk194g<Serial, D> {
     #[maybe_async::maybe_async]
     pub async fn write_buffer(&mut self, buffer: &[u8]) -> Result<(), JSYMk194Error> {
         let bytes_written = self.serial.write(buffer).await?;
-        println!(
-            "[Modbus] Raw request bytes   :  {:02X?}",
-            &buffer[..bytes_written]
-        );
+        // println!(
+        //     "[Modbus] Raw request bytes   :  {:02X?}",
+        //     &buffer[..bytes_written]
+        // );
         if bytes_written < buffer.len() {
             return Err(JSYMk194Error::FailedToWrite {
                 written: bytes_written,
@@ -23,10 +23,10 @@ impl<Serial: Read + Write, D: DelayNs> JsyMk194g<Serial, D> {
     #[maybe_async::maybe_async]
     pub async fn read_buffer(&mut self, buffer: &mut [u8]) -> Result<usize, JSYMk194Error> {
         let bytes_read = self.serial.read(buffer).await?;
-        println!(
-            "[Modbus] Raw response bytes  :  {:02X?}",
-            &buffer[..bytes_read]
-        );
+        // println!(
+        //     "[Modbus] Raw response bytes  :  {:02X?}",
+        //     &buffer[..bytes_read]
+        // );
         if bytes_read == ModbusErrorResponse::ERROR_RESPONSE_HEADER_SIZE {
             return Err(JSYMk194Error::ModBusDeviceError(
                 ModbusErrorResponse::from_bytes(&buffer[..bytes_read])?,
