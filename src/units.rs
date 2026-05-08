7458a8725c67e256afdd99b6b2f085bec7ee4b43
Scott Gibb <smgibb@yahoo.com>
2026-05-08 23:00:30 +0100

    docs: add missing units examples
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/units.rs
+++ src/units.rs
@@ -10,6 +10,8 @@
 //! - [`ElectricCurrent`] - Current measurements (amperes, milliamperes)
 //! - [`ElectricPotential`] - Voltage measurements (volts, millivolts)  
 //! - [`Power`] - Power measurements (watts)
+//! - [`Frequency`] - Line frequency measurements (hertz)
+//! - [`Energy`] - Energy measurements (watt-hours, kilowatt-hours)
 //!
 //! ## Unit Constants
 //!
@@ -18,6 +20,8 @@
 //! - **Current**: [`ampere`], [`milliampere`]
 //! - **Voltage**: [`volt`], [`millivolt`]
 //! - **Power**: [`watt`]
+//! - **Frequency**: [`hertz`]
+//! - **Energy**: [`watt_hour`], [`kilowatt_hour`]
 //!
 //! ## Usage Examples
 //!
@@ -27,10 +31,14 @@
 //! // Create measurements using unit constants
 //! let voltage = ElectricPotential::new::<volt>(5.0);
 //! let current = ElectricCurrent::new::<milliampere>(1500.0);
+//! let frequency = Frequency::new::<hertz>(50.0);
+//! let energy = Energy::new::<kilowatt_hour>(1.25);
 //!
 //! // Convert between units
 //! let voltage_mv = voltage.get::<millivolt>(); // 5000.0
 //! let current_a = current.get::<ampere>();     // 1.5
+//! let line_hz = frequency.get::<hertz>();       // 50.0
+//! let energy_wh = energy.get::<watt_hour>();    // 1250.0
 //! ```
 pub use uom::si::electric_current::ampere;
 pub use uom::si::electric_current::milliampere;
