49dc433189878f2e9bbdcdba8ef897f830792361
Scott Gibb <smgibb@yahoo.com>
2026-05-08 23:21:52 +0100

    docs: add delay description
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/modbus/protocol.rs
+++ src/modbus/protocol.rs
@@ -9,7 +9,10 @@ use crate::{
     registers::system_configuration_parameter::Id,
 };
 
+/// The JSY-MK-194 device has a minimum response time, this is used to ensure we wait long enoough,
+/// that the device can give us the correct information without erroring.
 pub const REQUEST_RESPONSE_DELAY: Duration = Duration::from_millis(400);
+// When requesting channel data, the device takes longer to respond, so we need to wait a bit longer.
 pub const CHANNEL_REQUEST_RESPONSE_DELAY: Duration = Duration::from_millis(800);
 // Verify that REQUEST_RESPONSE_DELAY can fit in a u32 when converted to milliseconds, since that's the type used in the driver implementation. This is important to prevent overflow issues when converting the duration to milliseconds.
 const _: () = assert!(
