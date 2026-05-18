c08cba3c35299dde32f1a0643e9cfe8e7f3965b5
Scott Gibb <smgibb@yahoo.com>
2026-05-18 21:24:23 +0100

    feat: add rp-hal example
    This example also doesnt work, it seems the sync option of the library
    does not work at all and thats what causing the stm32 example to also
    not work. More investigation needs done into the read and write calls to
    find out the solution.
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

---
+++ examples/rp2040/src/main.rs
@@ -0,0 +1,133 @@
+//! # UART Example
+//!
+//! This application demonstrates how to use the UART Driver to talk to a serial
+//! connection.
+//!
+//! It may need to be adapted to your particular board layout and/or pin
+//! assignment.
+//!
+//! See the top-level `README.md` file for Copyright and license details.
+
+#![no_std]
+#![no_main]
+
+use jsy_mk_194_rs::{
+    DEFAULT_CHANNEL_REQUEST_RESPONSE_DELAY, DEFAULT_REQUEST_RESPONSE_DELAY,
+    jsy_mk_194g::JsyMk194g,
+    types::{Baudrate, Id},
+};
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
+use panic_probe as _;
+
+// defmt-rtt must be linked to enable RTT logging via probe-rs
+use defmt_rtt as _;
+
+// Alias for our HAL crate
+use rp2040_hal::{self as hal};
+
+// A shorter alias for the Peripheral Access Crate, which provides low-level
+// register access
+use hal::pac;
+
+use hal::fugit::RateExtU32;
+use rp2040_hal::clocks::Clock;
+
+// UART related types
+use hal::uart::{DataBits, StopBits, UartConfig};
+
+/// The linker will place this boot block at the start of our program image. We
+/// need this to help the ROM bootloader get our code up and running.
+/// Note: This boot block is not necessary when using a rp-hal based BSP
+/// as the BSPs already perform this step.
+#[unsafe(link_section = ".boot2")]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
+/// if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// Entry point to our bare-metal application.
+///
+/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables and the spinlock are initialised.
+///
+/// The function configures the RP2040 peripherals, then writes to the UART in
+/// an infinite loop.
+#[rp2040_hal::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gpio0.into_function(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gpio1.into_function(),
+    );
+    defmt::info!("Configuring UART...");
+    let baudrate = Baudrate::default();
+    let baudrate = u32::from(baudrate);
+    defmt::info!("Setting baudrate to {} bps", baudrate);
+
+    let uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(baudrate.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+    defmt::info!("RP2040 Example");
+    defmt::info!("UART configured with baudrate: {}", Baudrate::default());
+    defmt::info!("Waiting for JSY-MK-194G startup...");
+    delay.delay_ms(1000);
+
+    defmt::info!("Setting up JSY-MK-194G driver");
+    let mut device = JsyMk194g::new(
+        uart,
+        Id::default(),
+        timer,
+        DEFAULT_REQUEST_RESPONSE_DELAY,
+        DEFAULT_CHANNEL_REQUEST_RESPONSE_DELAY,
+    );
+
+    loop {
+        defmt::info!("Scanning Channels...");
+        match device.get_all_channels() {
+            Ok(channels) => defmt::info!("Channels: {:?}", channels),
+            Err(err) => defmt::warn!("Channel read failed: {:?}", err),
+        }
+        delay.delay_ms(1000);
+    }
+}
+
+// End of file
